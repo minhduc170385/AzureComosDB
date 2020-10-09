fd4e00081f709a8a2c1134f71246b5376c50827e
Duc Bui <minhduc170385@gmail.com>
2020-10-08 20:24:40 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureComosDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
